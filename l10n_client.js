30216eaf9c84600322758ff03d1ebec1eb3935ee
Gábor Hojtsy <gabor@hojtsy.hu>
2007-10-30 18:44:07 +0000

    Improvements by Young Hahn:  - keyboard shortcuts for certain features  - search functionality in string list
    See http://www.developmentseed.org/blog/2007/oct/26/new-features-and-improved-workflow-l10n-client

--- l10n_client.js
+++ l10n_client.js
@@ -1,46 +1,97 @@
 // $Id$
 
-// Set "selected" string to unselected, i.e. -1
-jQuery.extend(Drupal, { l10nSelected: -1 });
-
-// Define method for toggling l10n_client window and saving its state
-// in a cookie.
-jQuery.extend(Drupal, { l10nClientToggle:
-  function(state) {
-    switch(state) {
-      case 1:
-        $('#l10n-client-string-select, #l10n-client-string-editor, #l10n-client .labels .label').show();
-        $('#l10n-client').height('22em').removeClass('hidden');
-        $('#l10n-client .labels .toggle').text('X');
-        if(!$.browser.msie) {
-          $('body').css('border-bottom', '22em solid #fff');
-        }
-        $.cookie('Drupal_l10n_client', '1', {expires: 7, path: '/'});
-      break;
-      case 0:
-        $('#l10n-client-string-select, #l10n-client-string-editor, #l10n-client .labels .label').hide();
-        $('#l10n-client').height('2em').addClass('hidden');
-        $('#l10n-client .labels .toggle').text('Translate Text');
-        if(!$.browser.msie) {
-          $('body').css('border-bottom', '0px');
-        }
-        $.cookie('Drupal_l10n_client', '0', {expires: 7, path: '/'});
-      break;        
+// Store all l10n_client related data + methods in its own object
+jQuery.extend(Drupal, {
+  l10nClient: new (function() {
+    // Set "selected" string to unselected, i.e. -1
+    this.selected = -1;
+    // Keybindings
+    this.keys = {'toggle':'ctrl+shift+s', 'clear': 'esc'}; // Keybindings
+    // Keybinding functions
+    this.key = function(pressed) {
+      switch(pressed) {
+        case 'toggle':
+          // Grab user-hilighted text & send it into the search filter
+          userSelection = window.getSelection ? window.getSelection() : document.getSelection ? document.getSelection() : document.selection.createRange().text;
+          userSelection = String(userSelection);
+          if(userSelection.length > 0) {
+            Drupal.l10nClient.filter(userSelection);
+            Drupal.l10nClient.toggle(1);
+            $('#l10n-client #edit-search').focus();      
+          } else {
+            if($('#l10n-client').is('.hidden')) {
+              Drupal.l10nClient.toggle(1);
+              if(!$.browser.safari) {
+                $('#l10n-client #edit-search').focus();
+              }
+            } else { 
+              Drupal.l10nClient.toggle(0);
+            }
+          }
+        break;
+        case 'clear':
+          this.filter(false);      
+        break;
+      }
     }
-  }
+    // Toggle the l10nclient
+    this.toggle = function(state) {
+      switch(state) {
+        case 1:
+          $('#l10n-client-string-select, #l10n-client-string-editor, #l10n-client .labels .label').show();
+          $('#l10n-client').height('22em').removeClass('hidden');
+          $('#l10n-client .labels .toggle').text('X');
+          if(!$.browser.msie) {
+            $('body').css('border-bottom', '22em solid #fff');
+          }
+          $.cookie('Drupal_l10n_client', '1', {expires: 7, path: '/'});
+        break;
+        case 0:
+          $('#l10n-client-string-select, #l10n-client-string-editor, #l10n-client .labels .label').hide();
+          $('#l10n-client').height('2em').addClass('hidden');
+          $('#l10n-client .labels .toggle').text('Translate Text');
+          if(!$.browser.msie) {
+            $('body').css('border-bottom', '0px');
+          }
+          $.cookie('Drupal_l10n_client', '0', {expires: 7, path: '/'});
+        break;        
+      }
+    }
+    // Get a string from the DOM tree
+    this.getString = function(index, type) {
+      return $('#l10n-client-data div:eq('+index+') .'+type).html();
+    }
+    // Set a string in the DOM tree
+    this.setString = function(index, data) {
+      $('#l10n-client-data div:eq('+index+') .target').html(data);
+    }
+    // Filter the the string list by a search string
+    this.filter = function(search) {
+      if(search == false || search == '') {
+        $('#l10n-client #search-filter-clear').focus();
+        $('#l10n-client-string-select li').show();
+        $('#l10n-client #edit-search').val('');
+        $('#l10n-client #edit-search').focus();
+      } else {
+        if(search.length > 0) {
+          $('#l10n-client-string-select li').hide();
+          $('#l10n-client-string-select li:contains('+search+')').show();
+          $('#l10n-client #edit-search').val(search);
+        }
+      }
+    }
+  })
 });
 
-/**
- * Attaches the localization editor behaviour to all required fields.
- */
-Drupal.behaviors.l10nEditor = function (context) {
+// Attaches the localization editor behaviour to all required fields.
+Drupal.behaviors.l10nClient = function (context) {
 
   switch($.cookie('Drupal_l10n_client')) {
     case '1':
-      Drupal.l10nClientToggle(1);
+      Drupal.l10nClient.toggle(1);
     break;
     default:
-      Drupal.l10nClientToggle(0);
+      Drupal.l10nClient.toggle(0);
     break;
   }
   
@@ -50,24 +101,21 @@ Drupal.behaviors.l10nEditor = function (context) {
     $('#l10n-client-string-select li').removeClass('active');
     $(this).addClass('active');
     var index = $('#l10n-client-string-select li').index(this);
-    $('#l10n-client-string-editor .source-text').text(Drupal.l10nStrings[index][0]);
-    $('#l10n-client-form #edit-target').val(Drupal.l10nStrings[index][1] == true ? '' : Drupal.l10nStrings[index][1]);
-    Drupal.l10nSelected = index;
+
+    $('#l10n-client-string-editor .source-text').text(Drupal.l10nClient.getString(index, 'source'));
+    $('#l10n-client-form #edit-target').val(Drupal.l10nClient.getString(index, 'target'));
+
+    Drupal.l10nClient.selected = index;
   });
 
   // When l10n_client window is clicked, toggle based on current state.
   $('#l10n-client .labels .toggle').click(function() {
     if($('#l10n-client').is('.hidden')) {
-      Drupal.l10nClientToggle(1);
+      Drupal.l10nClient.toggle(1);
     } else { 
-      Drupal.l10nClientToggle(0);
+      Drupal.l10nClient.toggle(0);
     }
   });
-
-  // Mark all strings depending on whether they are translated or not.
-  for (var i in Drupal.l10nStrings) {
-    $($('#l10n-client-string-select li')[i]).addClass(Drupal.l10nStrings[i][1] == true ? 'untranslated' : 'translated');
-  }
 
   // Copy source text to translation field on button click.
   $('#l10n-client-form #edit-copy').click(function() {
@@ -78,7 +126,23 @@ Drupal.behaviors.l10nEditor = function (context) {
   $('#l10n-client-form #edit-clear').click(function() {
     $('#l10n-client-form #edit-target').val('');
   });
+
+  // Register keybindings using jQuery hotkeys
+  if($.hotkeys) {
+    $.hotkeys.add(Drupal.l10nClient.keys['toggle'], function(){Drupal.l10nClient.key('toggle')});
+    $.hotkeys.add(Drupal.l10nClient.keys['clear'], {target:'#l10n-client #edit-search', type:'keyup'}, function(){Drupal.l10nClient.key('clear')});
+  }
   
+  // Custom listener for l10n_client livesearch
+  $('#l10n-client #edit-search').keyup(function(key) {
+    Drupal.l10nClient.filter($('#l10n-client #edit-search').val());
+  });
+
+  // Clear search
+  $('#l10n-client #search-filter-clear').click(function() {
+    Drupal.l10nClient.filter(false);
+  });
+
   // Send AJAX POST data on form submit.
   $('#l10n-client-form').submit(function() {
     $.ajax({
@@ -89,15 +153,15 @@ Drupal.behaviors.l10nEditor = function (context) {
             '&target=' + Drupal.encodeURIComponent($('#l10n-client-form #edit-target').val()),
       success: function (data) {
         // Store string in local js
-        Drupal.l10nStrings[Drupal.l10nSelected][1] = $('#l10n-client-form #edit-target').val();
+        Drupal.l10nClient.setString(Drupal.l10nClient.selected, $('#l10n-client-form #edit-target').val());
+
+        // Mark string as translated.
+        $('#l10n-client-string-select li').eq(Drupal.l10nClient.selected).removeClass('untranslated').removeClass('active').addClass('translated').text($('#l10n-client-form #edit-target').val());
 
         // Empty input fields.
         $('#l10n-client-string-editor .source-text').html('');
         $('#l10n-client-form #edit-target').val('');
 
-        // Mark string as translated.
-        $('#l10n-client-string-select li').eq(Drupal.l10nSelected).removeClass('untranslated').removeClass('active').addClass('translated');        
-        
       },
       error: function (xmlhttp) {
         alert(Drupal.t('An HTTP error @status occured.', { '@status': xmlhttp.status }));
@@ -105,5 +169,5 @@ Drupal.behaviors.l10nEditor = function (context) {
     });
     return false;
   });
-
+  
 };
