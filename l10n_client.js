9d93b8b30a1e7f414d2fe78ef9ab48748fb56c15
Gábor Hojtsy <gabor@hojtsy.hu>
2010-12-08 12:04:06 +0000

    #361147 by Jose Reyero, mathieu, neurovation.kiwi, jhedstrom, miro_dietiker, dereine and myself: port textgroup support to Drupal 7 finally

--- l10n_client.js
+++ l10n_client.js
@@ -114,6 +114,7 @@ Drupal.behaviors.l10nClient.attach = function (context) {
 
     $('#l10n-client-string-editor .source-text').text(Drupal.l10nClient.getString(index, 'source'));
     $('#l10n-client-form .translation-target').val(Drupal.l10nClient.getString(index, 'target'));
+    $('#l10n-client-form .source-textgroup').val(Drupal.l10nClient.getString(index, 'textgroup'));
 
     Drupal.l10nClient.selected = index;
     $('#l10n-client-form .form-submit').removeAttr("disabled");
@@ -167,6 +168,7 @@ Drupal.behaviors.l10nClient.attach = function (context) {
       data: {
         source: $('#l10n-client-string-editor .source-text').text(),
         target: $('#l10n-client-form .translation-target').val(),
+        textgroup: $('#l10n-client-form .source-textgroup').val(),
         'form_token': $('#l10n-client-form input[name=form_token]').val()
       },
       success: function (data) {
